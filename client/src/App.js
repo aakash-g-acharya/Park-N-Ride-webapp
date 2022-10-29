9ef4d28276c23c729c6f17b5f0d3cdc8851b36ce
aniketh-bellala <aniketh.201cs108@nitk.edu.in>
2022-10-29 21:49:46 +0530

    comment in app.js client line 8

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,19 @@ import Main from "./components/Main";
 import Signup from "./components/Singup";
 import Login from "./components/Login";
 
-function App() {
-	const user = localStorage.getItem("token");
+//
 
-	return (
-		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}
-			<Route path="/signup" exact element={<Signup />} />
-			<Route path="/login" exact element={<Login />} />
-			<Route path="/" element={<Navigate replace to="/login" />} />
-		</Routes>
-	);
+function App() {
+  const user = localStorage.getItem("token");
+
+  return (
+    <Routes>
+      {user && <Route path="/" exact element={<Main />} />}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/" element={<Navigate replace to="/login" />} />
+    </Routes>
+  );
 }
 
 export default App;
